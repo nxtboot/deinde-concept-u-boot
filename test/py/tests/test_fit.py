04f200de3f39b453b4ebb75065689b276f4bf0d3
Simon Glass <sjg@chromium.org>
2026-04-13 06:59:52 -0600

    test: fit: Drop the fit_signature buildconfigspec
    The test_fit test requires buildconfigspec('fit_signature') but does
    not exercise signature verification. The ITS template includes a
    signature-1 node, but mkimage only needs TOOLS_FIT_SIGNATURE (a host
    tool option) to handle it, not CONFIG_FIT_SIGNATURE in the U-Boot
    binary.
    
    Since sandbox does not enable CONFIG_FIT_SIGNATURE, the test is silently
    skipped on every run. Change the marker to buildconfigspec('fit') which
    is all the test actually needs.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    (cherry picked from commit 863446256b0b03a1729c697dc694014c74cbd0be)

--- test/py/tests/test_fit.py
+++ test/py/tests/test_fit.py
@@ -118,7 +118,7 @@ host save hostfs 0 %(loadables2_addr)x %(loadables2_out)s %(loadables2_size)x
 '''
 
 @pytest.mark.boardspec('sandbox')
-@pytest.mark.buildconfigspec('fit_signature')
+@pytest.mark.buildconfigspec('fit')
 @pytest.mark.requiredtool('dtc')
 class TestFitImage:
     """Test class for FIT image handling in U-Boot
