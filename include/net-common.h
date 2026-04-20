b3ec9f77cc6cbfe6fc7887ec4fed2349abfc7029
Quentin Schulz <quentin.schulz@cherry.de>
2026-04-20 13:36:10 +0200

    simplify NET_LEGACY || NET_LWIP condition with NET condition
    Since the move to make NET a menuconfig and NO_NET a synonym of NET=n,
    when NET is enabled, NET_LEGACY || NET_LWIP is necessarily true, so
    let's simplify the various checks across the codebase.
    
    SPL_NET_LWIP doesn't exist but SPL_NET_LEGACY is an alias for SPL_NET so
    the proper symbol is still defined in SPL whenever needed.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@cherry.de>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Peter Robinson <pbrobinson@gmail.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>
    (cherry picked from commit 95d66d2eb02a4677c63d04c84ca21750a04c49f1)
    
    [sjg: Resolve conflicts in board_r.c (LED init), efi_loader Kconfig (MACH_QEMU)]

--- include/net-common.h
+++ include/net-common.h
@@ -237,7 +237,7 @@ int eth_rx(void);			/* Check for received packets */
  */
 void reset_phy(void);
 
-#if CONFIG_IS_ENABLED(NET_LEGACY) || CONFIG_IS_ENABLED(NET_LWIP)
+#if CONFIG_IS_ENABLED(NET)
 /**
  * eth_set_enable_bootdevs() - Enable or disable binding of Ethernet bootdevs
  *
