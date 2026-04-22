u-boot/u-boot

video: add DejaVu Mono font
A TrueType font for U-Boot should fulfill the following requirements:

* mono spaced
* support full code page 437
* easily readable

Unfortunately none of the fonts provided with U-Boot fulfills all of these
requirements.

Let's add the DejaVu Mono font. To reduce the code size the characters are
limited to code page 437.

Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

drivers/video/console_truetype.c
@@ -528,6 +528,7 @@ FONT_DECL(nimbus_sans_l_regular);
 FONT_DECL(ankacoder_c75_r);
 FONT_DECL(rufscript010);
 FONT_DECL(cantoraone_regular);
+FONT_DECL(dejavu_mono);
 
 static struct font_info font_table[] = {
 #ifdef CONFIG_CONSOLE_TRUETYPE_NIMBUS
@@ -541,6 +542,9 @@ static struct font_info font_table[] = {
 #endif
 #ifdef CONFIG_CONSOLE_TRUETYPE_CANTORAONE
 	FONT_ENTRY(cantoraone_regular),
+#endif
+#ifdef CONFIG_CONSOLE_TRUETYPE_DEJAVU
+	FONT_ENTRY(dejavu_mono),
 #endif
 	{} /* sentinel */
 };

nxtboot/deinde-concept-u-boot

(cherry picked from commit 3a9795581eb7d2c7978802d2d91832ce00e0efe4)
Fix-up: resolve conflicts with local Ubuntu font entries and use TT_FONT_ENTRY macro.

drivers/video/console_truetype.c
@@ -740,6 +740,7 @@ FONT_DECL(rufscript010);
 FONT_DECL(cantoraone_regular);
 FONT_DECL(ubuntu_light);
 FONT_DECL(ubuntu_bold);
+FONT_DECL(dejavu_mono);
 
 static struct font_info font_table[] = {
 #ifdef CONFIG_CONSOLE_TRUETYPE_NIMBUS
@@ -759,6 +760,9 @@ static struct font_info font_table[] = {
 #endif
 #ifdef CONFIG_CONSOLE_TRUETYPE_UBUNTU_BOLD
 	TT_FONT_ENTRY(ubuntu_bold),
+#endif
+#ifdef CONFIG_CONSOLE_TRUETYPE_DEJAVU
+	TT_FONT_ENTRY(dejavu_mono),
 #endif
 	{} /* sentinel */
 };