sjg20/u-boot-concept

x86: Fix up some Kconfig options
There are a few inconsistencies in various options on x86. Fix these up
so that conversion to a split config is possible:

- use Proper config in irq, timer and environment
- drop the invalid SPL_ in a CONFIG_IS_ENABLED() usage
- use the correct X86_64 option in msr.h

Signed-off-by: Simon Glass <sjg@chromium.org>

drivers/timer/tsc_timer.c
@@ -376,7 +376,7 @@ void __udelay(unsigned long usec)
 	stop = now + (u64)usec * get_tbclk_mhz();
 
 	while ((int64_t)(stop - get_ticks()) > 0)
-#if defined(CONFIG_QEMU) && defined(CONFIG_SMP)
+#if defined(CONFIG_QEMU) && defined(CONFIG_PPL_SMP)
 		/*
 		 * Add a 'pause' instruction on qemu target,
 		 * to give other VCPUs a chance to run.

nxtboot/deinde-concept-u-boot

(cherry picked from commit f887b9dc0b848b798fb0eba30bff079d61e4a1d9)

drivers/timer/tsc_timer.c
@@ -384,7 +384,7 @@ void __udelay(unsigned long usec)
 	stop = now + (u64)usec * get_tbclk_mhz();
 
 	while ((int64_t)(stop - get_ticks()) > 0)
-#if defined(CONFIG_ARCH_QEMU_X86) && defined(CONFIG_SMP)
+#if defined(CONFIG_ARCH_QEMU_X86) && defined(CONFIG_PPL_SMP)
 		/*
 		 * Add a 'pause' instruction on qemu target,
 		 * to give other VCPUs a chance to run.