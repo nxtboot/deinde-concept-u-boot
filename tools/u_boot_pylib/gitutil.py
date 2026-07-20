88744e9d03607de4cdd0fccbe78b631e03d8b4e1
Neil Armstrong <neil.armstrong@linaro.org>
2026-07-20 10:23:31 +0200

    MAINTAINERS/README: Migrate mailing-list to lists.u-boot-project.org
    The U-Boot Mailing-list is moving to the lists.u-boot-project.org
    domain, so update all references of list.denx.de to the new
    domain in the main README and MAINTAINERS files.
    
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>
    Reviewed-by: Peter Robinson <pbrobinson@gmail.com>
    (cherry picked from commit 910385cacb8878007da2011bbf8733ee30bab7f9)
    
    Fixup: kept ci/master's (None, 0) return signature in gitutil.py email_patches while updating the URL string

--- tools/u_boot_pylib/gitutil.py
+++ tools/u_boot_pylib/gitutil.py
@@ -937,7 +937,7 @@ def email_patches(series, cover_fname, args, dry_run, warn_on_error, cc_fname,
                   "Please add something like this to a commit\n"
                   "Series-to: Fred Bloggs <f.blogs@napier.co.nz>\n"
                   "Or do something like this\n"
-                  "git config sendemail.to u-boot@lists.denx.de")
+                  "git config sendemail.to u-boot@lists.u-boot-project.org")
             return None, 0
     cc = build_email_list(list(set(series.get('cc')) - set(series.get('to'))),
                           alias, '--cc', warn_on_error)
