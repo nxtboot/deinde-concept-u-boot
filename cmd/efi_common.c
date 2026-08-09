u-boot/u-boot

efi: Unify the memory map output of 'efi mem' and 'efidebug memmap'
The efi and efidebug commands each carried their own code for printing
the EFI memory map, with separate tables of memory type and attribute
names. The copies had drifted: efidebug knew EFI_PERSISTENT_MEMORY_TYPE
while 'efi mem' printed it as '<invalid>', neither table knew
EFI_UNACCEPTED_MEMORY_TYPE, and the 'efi mem' printer had misaligned
column headers, a broken '<gap>' line and a superfluous Virtual column:
the map is identity mapped before SetVirtualAddressMap() is called, so
the field carries no information at the time the command can run.

Move the printing loop of 'efidebug memmap' into efi_common.c as
efi_show_memmap(), which is linked into both commands, and use it from
both. The second copy in 'efi mem' is deleted together with
efi_print_mem_table() and the private sorting and merging code,
including the 'all' argument.

The memory type names follow the UEFI specification with the leading
'Efi' and the trailing 'Type' stripped, for example ConventionalMemory
for EfiConventionalMemory, and the missing name for unaccepted memory
is added. The type column is widened to fit the longest name,
MemoryMappedIOPortSpace.

The shared function iterates the map with the descriptor size reported
by the firmware instead of assuming sizeof(struct efi_mem_desc). This
matters for 'efi mem' under EDK II based firmware, which reports a
descriptor size of 0x30. The memory map key, which was printed
uninitialized on the payload path, is now initialized. The command
documentation is updated with output captured from the app running
under OVMF, and documents why virtual addresses are not shown.

Suggested-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
Signed-off-by: Aristo Chen <aristo.chen@canonical.com>

cmd/efi_common.c
@@ -8,7 +8,58 @@
 
 #include <efi.h>
 #include <efi_api.h>
+#include <mapmem.h>
 #include <u-boot/uuid.h>
+
+/* Width of an EFI physical address in hexadecimal digits */
+#define EFI_PHYS_ADDR_WIDTH (int)(sizeof(efi_physical_addr_t) * 2)
+
+/* Width of the memory type column, sized for the longest type name */
+#define EFI_MEM_TYPE_WIDTH 23
+
+/*
+ * The type names of the UEFI specification without the leading 'Efi' and
+ * the trailing 'Type'
+ */
+static const char *const efi_mem_type_string[] = {
+	[EFI_RESERVED_MEMORY_TYPE] = "ReservedMemory",
+	[EFI_LOADER_CODE] = "LoaderCode",
+	[EFI_LOADER_DATA] = "LoaderData",
+	[EFI_BOOT_SERVICES_CODE] = "BootServicesCode",
+	[EFI_BOOT_SERVICES_DATA] = "BootServicesData",
+	[EFI_RUNTIME_SERVICES_CODE] = "RuntimeServicesCode",
+	[EFI_RUNTIME_SERVICES_DATA] = "RuntimeServicesData",
+	[EFI_CONVENTIONAL_MEMORY] = "ConventionalMemory",
+	[EFI_UNUSABLE_MEMORY] = "UnusableMemory",
+	[EFI_ACPI_RECLAIM_MEMORY] = "ACPIReclaimMemory",
+	[EFI_ACPI_MEMORY_NVS] = "ACPIMemoryNVS",
+	[EFI_MMAP_IO] = "MemoryMappedIO",
+	[EFI_MMAP_IO_PORT] = "MemoryMappedIOPortSpace",
+	[EFI_PAL_CODE] = "PalCode",
+	[EFI_PERSISTENT_MEMORY_TYPE] = "PersistentMemory",
+	[EFI_UNACCEPTED_MEMORY_TYPE] = "UnacceptedMemory",
+};
+
+static const struct efi_mem_attrs {
+	const u64 bit;
+	const char *text;
+} efi_mem_attrs[] = {
+	{EFI_MEMORY_UC, "UC"},
+	{EFI_MEMORY_WC, "WC"},
+	{EFI_MEMORY_WT, "WT"},
+	{EFI_MEMORY_WB, "WB"},
+	{EFI_MEMORY_UCE, "UCE"},
+	{EFI_MEMORY_WP, "WP"},
+	{EFI_MEMORY_RP, "RP"},
+	{EFI_MEMORY_XP, "XP"},
+	{EFI_MEMORY_NV, "NV"},
+	{EFI_MEMORY_MORE_RELIABLE, "REL"},
+	{EFI_MEMORY_RO, "RO"},
+	{EFI_MEMORY_SP, "SP"},
+	{EFI_MEMORY_CPU_CRYPTO, "CRYPT"},
+	{EFI_MEMORY_HOT_PLUGGABLE, "HOTPL"},
+	{EFI_MEMORY_RUNTIME, "RT"},
+};
 
 void efi_show_tables(struct efi_system_table *systab)
 {
@@ -19,5 +70,75 @@ void efi_show_tables(struct efi_system_table *systab)
 
 		printf("%p  %pUl  %s\n", tab->table, tab->guid.b,
 		       uuid_guid_get_str(tab->guid.b) ?: "(unknown)");
+	}
+}
+
+/**
+ * efi_mem_type_name() - get the name of an EFI memory type
+ *
+ * @type: memory type (enum efi_memory_type)
+ * Return: name of the memory type, or NULL if @type is unknown
+ */
+static const char *efi_mem_type_name(u32 type)
+{
+	if (type >= ARRAY_SIZE(efi_mem_type_string))
+		return NULL;
+
+	return efi_mem_type_string[type];
+}
+
+/**
+ * efi_print_mem_attrs() - print the names of set EFI memory attributes
+ *
+ * Prints the set attribute bits as a '|'-separated list of mnemonics,
+ * e.g. ' UC|WB|RT', preceded by a space. Prints nothing if no known
+ * attribute bit is set.
+ *
+ * @attributes: memory attributes (EFI_MEMORY_...)
+ */
+static void efi_print_mem_attrs(u64 attributes)
+{
+	int sep, i;
+
+	for (sep = 0, i = 0; i < ARRAY_SIZE(efi_mem_attrs); i++)
+		if (attributes & efi_mem_attrs[i].bit) {
+			if (sep) {
+				putc('|');
+			} else {
+				putc(' ');
+				sep = 1;
+			}
+			puts(efi_mem_attrs[i].text);
+		}
+}
+
+void efi_show_memmap(struct efi_mem_desc *map, efi_uintn_t map_size,
+		     efi_uintn_t desc_size)
+{
+	struct efi_mem_desc *end = (void *)map + map_size;
+	static const char sep[] = "========================";
+	const char *type;
+
+	printf("%-*s %-*s %-*s Attributes\n",
+	       EFI_MEM_TYPE_WIDTH, "Type",
+	       EFI_PHYS_ADDR_WIDTH, "Start", EFI_PHYS_ADDR_WIDTH, "End");
+	printf("%.*s %.*s %.*s ==========\n",
+	       EFI_MEM_TYPE_WIDTH, sep,
+	       EFI_PHYS_ADDR_WIDTH, sep, EFI_PHYS_ADDR_WIDTH, sep);
+
+	for (; map < end; map = efi_get_next_mem_desc(map, desc_size)) {
+		type = efi_mem_type_name(map->type) ?: "(unknown)";
+
+		printf("%-*s %.*llx-%.*llx", EFI_MEM_TYPE_WIDTH, type,
+		       EFI_PHYS_ADDR_WIDTH,
+		       (u64)map_to_sysmem((void *)(uintptr_t)
+					  map->physical_start),
+		       EFI_PHYS_ADDR_WIDTH,
+		       (u64)map_to_sysmem((void *)(uintptr_t)
+					  (map->physical_start +
+					   map->num_pages * EFI_PAGE_SIZE)));
+
+		efi_print_mem_attrs(map->attribute);
+		putc('\n');
 	}
 }

nxtboot/deinde-concept-u-boot

(cherry picked from commit 6abd738e20dd09bdebc9e2f9ad6162cc4b1c678e)
[pickman] Adapt the refactor to this tree, which has diverged in these
files. Keep the local 'efi image' and 'efi memsync' subcommands and the
is_app() helper. Keep the '-s' flag of 'efidebug memmap' from
2a374c390e03, sorting the map before handing it to efi_show_memmap(),
and pass desc_size rather than sizeof(*memmap) so the EFI_APP path keeps
working. Rename the new static efi_mem_type_name() to
efi_memmap_type_name(), since d07934b67e2f already provides a global
efi_mem_type_name() with different names and three callers.

cmd/efi_common.c
@@ -8,7 +8,58 @@
 
 #include <efi.h>
 #include <efi_api.h>
+#include <mapmem.h>
 #include <u-boot/uuid.h>
+
+/* Width of an EFI physical address in hexadecimal digits */
+#define EFI_PHYS_ADDR_WIDTH (int)(sizeof(efi_physical_addr_t) * 2)
+
+/* Width of the memory type column, sized for the longest type name */
+#define EFI_MEM_TYPE_WIDTH 23
+
+/*
+ * The type names of the UEFI specification without the leading 'Efi' and
+ * the trailing 'Type'
+ */
+static const char *const efi_mem_type_string[] = {
+	[EFI_RESERVED_MEMORY_TYPE] = "ReservedMemory",
+	[EFI_LOADER_CODE] = "LoaderCode",
+	[EFI_LOADER_DATA] = "LoaderData",
+	[EFI_BOOT_SERVICES_CODE] = "BootServicesCode",
+	[EFI_BOOT_SERVICES_DATA] = "BootServicesData",
+	[EFI_RUNTIME_SERVICES_CODE] = "RuntimeServicesCode",
+	[EFI_RUNTIME_SERVICES_DATA] = "RuntimeServicesData",
+	[EFI_CONVENTIONAL_MEMORY] = "ConventionalMemory",
+	[EFI_UNUSABLE_MEMORY] = "UnusableMemory",
+	[EFI_ACPI_RECLAIM_MEMORY] = "ACPIReclaimMemory",
+	[EFI_ACPI_MEMORY_NVS] = "ACPIMemoryNVS",
+	[EFI_MMAP_IO] = "MemoryMappedIO",
+	[EFI_MMAP_IO_PORT] = "MemoryMappedIOPortSpace",
+	[EFI_PAL_CODE] = "PalCode",
+	[EFI_PERSISTENT_MEMORY_TYPE] = "PersistentMemory",
+	[EFI_UNACCEPTED_MEMORY_TYPE] = "UnacceptedMemory",
+};
+
+static const struct efi_mem_attrs {
+	const u64 bit;
+	const char *text;
+} efi_mem_attrs[] = {
+	{EFI_MEMORY_UC, "UC"},
+	{EFI_MEMORY_WC, "WC"},
+	{EFI_MEMORY_WT, "WT"},
+	{EFI_MEMORY_WB, "WB"},
+	{EFI_MEMORY_UCE, "UCE"},
+	{EFI_MEMORY_WP, "WP"},
+	{EFI_MEMORY_RP, "RP"},
+	{EFI_MEMORY_XP, "XP"},
+	{EFI_MEMORY_NV, "NV"},
+	{EFI_MEMORY_MORE_RELIABLE, "REL"},
+	{EFI_MEMORY_RO, "RO"},
+	{EFI_MEMORY_SP, "SP"},
+	{EFI_MEMORY_CPU_CRYPTO, "CRYPT"},
+	{EFI_MEMORY_HOT_PLUGGABLE, "HOTPL"},
+	{EFI_MEMORY_RUNTIME, "RT"},
+};
 
 void efi_show_tables(struct efi_system_table *systab)
 {
@@ -19,5 +70,75 @@ void efi_show_tables(struct efi_system_table *systab)
 
 		printf("%p  %pUl  %s\n", tab->table, tab->guid.b,
 		       uuid_guid_get_str(tab->guid.b) ?: "(unknown)");
+	}
+}
+
+/**
+ * efi_memmap_type_name() - get the name of an EFI memory type
+ *
+ * @type: memory type (enum efi_memory_type)
+ * Return: name of the memory type, or NULL if @type is unknown
+ */
+static const char *efi_memmap_type_name(u32 type)
+{
+	if (type >= ARRAY_SIZE(efi_mem_type_string))
+		return NULL;
+
+	return efi_mem_type_string[type];
+}
+
+/**
+ * efi_print_mem_attrs() - print the names of set EFI memory attributes
+ *
+ * Prints the set attribute bits as a '|'-separated list of mnemonics,
+ * e.g. ' UC|WB|RT', preceded by a space. Prints nothing if no known
+ * attribute bit is set.
+ *
+ * @attributes: memory attributes (EFI_MEMORY_...)
+ */
+static void efi_print_mem_attrs(u64 attributes)
+{
+	int sep, i;
+
+	for (sep = 0, i = 0; i < ARRAY_SIZE(efi_mem_attrs); i++)
+		if (attributes & efi_mem_attrs[i].bit) {
+			if (sep) {
+				putc('|');
+			} else {
+				putc(' ');
+				sep = 1;
+			}
+			puts(efi_mem_attrs[i].text);
+		}
+}
+
+void efi_show_memmap(struct efi_mem_desc *map, efi_uintn_t map_size,
+		     efi_uintn_t desc_size)
+{
+	struct efi_mem_desc *end = (void *)map + map_size;
+	static const char sep[] = "========================";
+	const char *type;
+
+	printf("%-*s %-*s %-*s Attributes\n",
+	       EFI_MEM_TYPE_WIDTH, "Type",
+	       EFI_PHYS_ADDR_WIDTH, "Start", EFI_PHYS_ADDR_WIDTH, "End");
+	printf("%.*s %.*s %.*s ==========\n",
+	       EFI_MEM_TYPE_WIDTH, sep,
+	       EFI_PHYS_ADDR_WIDTH, sep, EFI_PHYS_ADDR_WIDTH, sep);
+
+	for (; map < end; map = efi_get_next_mem_desc(map, desc_size)) {
+		type = efi_memmap_type_name(map->type) ?: "(unknown)";
+
+		printf("%-*s %.*llx-%.*llx", EFI_MEM_TYPE_WIDTH, type,
+		       EFI_PHYS_ADDR_WIDTH,
+		       (u64)map_to_sysmem((void *)(uintptr_t)
+					  map->physical_start),
+		       EFI_PHYS_ADDR_WIDTH,
+		       (u64)map_to_sysmem((void *)(uintptr_t)
+					  (map->physical_start +
+					   map->num_pages * EFI_PAGE_SIZE)));
+
+		efi_print_mem_attrs(map->attribute);
+		putc('\n');
 	}
 }